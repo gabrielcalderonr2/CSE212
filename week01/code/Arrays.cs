3abac773808c304d691b310d8527bb333bdee974
gabrielcalderonr <cal10018@byui.edu>
2026-03-10 00:50:46 -0500

    Completed W01 Dynamic Arrays

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -8,12 +8,21 @@ public static class Arrays
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
     public static double[] MultiplesOf(double number, int length)
     {
-        // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        // Plan:
+        // 1. Create an array with the size of length
+        // 2. Use a loop to go through each position
+        // 3. Multiply the number by (index + 1)
+        // 4. Save the result in the array
+        // 5. Return the array
 
-        return []; // replace this return statement with your own
+        double[] result = new double[length];
+
+        for (int i = 0; i < length; i++)
+        {
+            result[i] = number * (i + 1);
+        }
+
+        return result;
     }
 
     /// <summary>
@@ -25,9 +34,18 @@ public static class Arrays
     /// </summary>
     public static void RotateListRight(List<int> data, int amount)
     {
-        // TODO Problem 2 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        // Plan:
+        // 1. Find where the last 'amount' elements start in the list
+        // 2. Get those elements using GetRange
+        // 3. Remove them from the original list
+        // 4. Insert them at the beginning of the list
+
+        int startIndex = data.Count - amount;
+
+        List<int> moved = data.GetRange(startIndex, amount);
+
+        data.RemoveRange(startIndex, amount);
+
+        data.InsertRange(0, moved);
     }
 }
